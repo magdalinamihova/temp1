5dc00b6fb05a15e08a6022455dd6960539001b9e
magdalinamihova <mih22040@spengergasse.at>
2023-09-28 23:41:42 +0200

    Initializing spring maven package

---
+++ src/test/java/at/spengergasse/sj2324posproject/Sj2324PosProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.sj2324posproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sj2324PosProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
