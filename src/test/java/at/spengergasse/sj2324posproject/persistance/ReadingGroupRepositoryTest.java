7c845039cca988068d7e6f1e5fb87b8c6d065ce3
magdalinamihova <mih22040@spengergasse.at>
2023-11-18 13:57:30 +0100

    ReadingGroupRepositoryTest implemented and functioning

--- src/test/java/at/spengergasse/sj2324posproject/persistance/ReadingGroupRepositoryTest.java
+++ src/test/java/at/spengergasse/sj2324posproject/persistance/ReadingGroupRepositoryTest.java
@@ -1,8 +1,38 @@
 package at.spengergasse.sj2324posproject.persistance;
 
-import static org.junit.jupiter.api.Assertions.*;
+import at.spengergasse.sj2324posproject.domain.entities.ReadingGroup;
+import at.spengergasse.sj2324posproject.domain.entities.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static at.spengergasse.sj2324posproject.persistance.domain.TestFixtures.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class ReadingGroupRepositoryTest {
 
+    @Autowired
+    private UserRepository userRepository;
 
+    @Autowired
+    private ReadingGroupRepository readingGroupRepository;
+
+    @BeforeEach
+    void setup() {
+        User savedUser = userRepository.save(user());
+        ReadingGroup readingGroup = readingGroup(savedUser);
+        readingGroupRepository.save(readingGroup);
+    }
+
+    @Test
+    void ensureSavingAndRereadingReadingGroupWorks() {
+        assertThat(readingGroupRepository).isNotNull();
+    }
+
+    @Test
+    void ensureFindByNameWorks() {
+        assertThat(readingGroupRepository.findByName("Book Club 1")).isPresent();
+    }
 }
