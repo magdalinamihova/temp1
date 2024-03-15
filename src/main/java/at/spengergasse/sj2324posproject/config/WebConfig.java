808d45ae1a34734035d95d999a0e42ed71a879b8
Emelie <126779598+Emelie198@users.noreply.github.com>
2024-03-15 09:09:43 +0100

    Upgraded springframework to 3.2.3

--- src/main/java/at/spengergasse/sj2324posproject/config/WebConfig.java
+++ src/main/java/at/spengergasse/sj2324posproject/config/WebConfig.java
@@ -1,38 +1,23 @@
 package at.spengergasse.sj2324posproject.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
-//import org.springframework.web.client.RestClient;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
 
-// #todo RestClient -> update spring-boot!
-// #todo then ged rid of restService class
 @Configuration
 public class WebConfig {
-    //public static final String BASE_URL = "https://httpbin.org";
+    public static final String BASE_URL = "https://httpbin.org";
 
-    // #todo RestClient
 
-    // RestClient & JdkClientHttpRequestFactory not under spring-boot-3.1.4 -> 3.2.2
-    /*@Bean
+    @Bean
     public RestClient httpBin(RestClient.Builder builder, @Value("${httpbin.org.uri}") String httpBinUri) {
         return builder.requestFactory(new JdkClientHttpRequestFactory())
                 .baseUrl(httpBinUri)
                 .build();
-    }*/
-
-
-    // useless
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate(new SimpleClientHttpRequestFactory());
     }
 
-    /*
-    @Bean
-    public RestService restService(RestTemplate restTemplate) {
-        return new RestService(restTemplate);
-    }*/
+    // #todo look up if further code needed
 
 }
---
+++ src/main/java/at/spengergasse/sj2324posproject/presentation/www/UserController.java
@@ -0,0 +1,5 @@
+package at.spengergasse.sj2324posproject.presentation.www;
+
+
+public class UserController {
+}
