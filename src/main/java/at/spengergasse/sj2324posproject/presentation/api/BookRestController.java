7d436f0068645092754235953bbe8ca3b859c675
magdalinamihova <mih22040@spengergasse.at>
2024-03-29 13:25:14 +0100

    Migration checksum repaire

---
+++ src/main/java/at/spengergasse/sj2324posproject/presentation/api/BookRestController.java
@@ -0,0 +1,14 @@
+package at.spengergasse.sj2324posproject.presentation.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookRestController {
+    @GetMapping
+    public String getBooks(){
+        return "Here are your books";
+    }
+}
